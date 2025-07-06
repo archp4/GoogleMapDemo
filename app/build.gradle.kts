6dde9e9f2b9ce01de582930a7f5c94a39fdc83ca
IshikaBhatia24 <bhatiaishika2402@gmail.com>
2025-07-05 22:08:49 -0400

    Markers added for North campus, Downtown campus, Lakeshore campus and harbourfront

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/archblog/googlemapdemo/MainActivity.java
+++ app/src/main/java/com/archblog/googlemapdemo/MainActivity.java
@@ -1,24 +1,51 @@
 package com.archblog.googlemapdemo;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //North campus
+        LatLng northCampus = new LatLng(43.7282, -79.6071);
+        mMap.addMarker(new MarkerOptions().position(northCampus).title("Humber North Campus"));
+
+        //Downtown campus
+        LatLng downtownCampus = new LatLng(43.67002727262021, -79.38381026436184);
+        mMap.addMarker(new MarkerOptions().position(downtownCampus).title("Humber Downtown Campus"));
+
+        //Lakeshore campus
+        LatLng lakeshoreCampus = new LatLng(43.59632559137166, -79.52011875565697);
+        mMap.addMarker(new MarkerOptions().position(lakeshoreCampus).title("Humber Lakeshore Campus"));
+
+        //Harbourfront
+        LatLng harbourfront = new LatLng(43.6387, -79.3801);
+        mMap.addMarker(new MarkerOptions().position(harbourfront).title("Harbourfront"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(northCampus, 12));
     }
 }
